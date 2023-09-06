7064b2955ecf6343c16c972b590aab19afeb1ef0
Bravo <zoaal22@gmail.com>
2023-09-06 19:51:59 +0900

    Before first Merge

--- YoutubeProject/Screens/Main/MainVC.swift
+++ YoutubeProject/Screens/Main/MainVC.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class MainVC: UIViewController {
     
+    let containerView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        configureContainerView()
+        addVCS()
+        
+    }
+    
+    private func addVCS() {
+        let vc = MainCollectionVC(collectionViewLayout: UICollectionViewLayout())
+        add(vc, to: containerView)
+    }
+    
+    private func configureContainerView() {
+        
+        view.addSubview(containerView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
     }
 }
--- YoutubeProject/Supports/SceneDelegate.swift
+++ YoutubeProject/Supports/SceneDelegate.swift
@@ -58,5 +58,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         return vc
     }
     
+   
+   
 }
 
